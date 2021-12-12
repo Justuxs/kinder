fa364645704239d102e870532909b22f7c71b2d5
Justasban <justas.ban2001@gmail.com>
2021-12-12 16:45:39 +0200

    logging URL with middleware

---
+++ Middleware/LogUrlMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Serilog;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.Hosting;
+
+namespace kinder_app.Middleware
+{
+    // REQUIREMENT: middleware 2
+    public class LogUrlMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public LogUrlMiddleware(RequestDelegate next, ILogger logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // REQUIREMENT: logging 2
+            _logger.Information($"Request URL: {Microsoft.AspNetCore.Http.Extensions.UriHelper.GetDisplayUrl(context.Request)}");
+
+            await this._next(context);
+        }
+
+    }
+}
