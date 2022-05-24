3cb2e751832b67c5a59d942a8a9e972d100929f4
Rokas <Rokas@LAPTOP-5NAFTNSP>
2022-05-24 19:48:37 +0300

    Tried to add Integration Tests

---
+++ IntegrationTests/HomeControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTests
+{
+    /*
+    public class HomeControllerTests : IntegrationTest
+    {
+        public async Task LikeThis_ownerN_is_null()
+        {
+            // Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+    */
+}
---
+++ IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,41 @@
+﻿using kinder_app;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTests
+{
+    /*
+    public class IntegrationTest
+    {
+        protected readonly HttpClient TestClient;
+        protected IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        services.Remove(typeof(DataContext));
+                    });
+
+                });
+            TestClient = appFactory.CreateClient();
+        }
+
+        protected async Task AuthenticateAsync()
+        {
+            TestClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue ("bearer", await GetJwtAsync());
+        }
+        private async Task<String> GetJwtAsync()
+        {
+            
+        }
+    }
+    */
+}
